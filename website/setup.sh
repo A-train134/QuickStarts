a11b70240b014a26d9b350691364e98d3388d216
a-train134 <janthonyhitch@gmail.com>
2026-05-22 05:02:30 -0400

    a little touch up

--- website/setup.sh
+++ website/setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 set -e
-site_name="quickstart"
-while getopts "n:o" opt; do 
+site_name="quickstart_site"
+while getopts "noc" opt; do 
 	case $opt in
 	n) # nginx on ubuntu
 		echo "Testing and Reloading nginx..."
 		if [ ! -L /etc/nginx/sites-enabled/${site_name}.conf ]; then
 			echo "No Simlink"
-			ln -s /etc/nginx/sites-available/basic_site.conf /etc/nginx/sites-enabled/basic_site.conf
+			ln -s /etc/nginx/sites-available/${site_name}.conf /etc/nginx/sites-enabled/${site_name}.conf
 			echo "Made Simlink"
 		fi
 		cp -f ./basic_site.conf /etc/nginx/sites-available/ 
@@ -22,13 +22,20 @@ while getopts "n:o" opt; do
 		nginx -s reload
 		echo "Done"
 		;;
+	c)
+		echo "Copying Files..."
+		cp -rf ./basic_site/ /var/www/${site_name}
+		echo "Finished Copying"
+		;;
 	\?)
 		echo "Invalid Option" >&2
 		exit 1
 		;;
 	esac
 done
-echo "Copying Files..."
-cp -rf ./basic_site/ /var/www/${site_name}
-echo "Finished Copying"
+# echo "Options"
+# echo "-u handle nginx on [u]buntu"
+# echo "-a handle nginx on [a]rch/ubuntu"
+# echo "-c copy site files to /var/www/."
+echo "Done"
 exit 0
