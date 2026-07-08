f7a8b175e6fc771bbe3e40893dcbdba02d5e16d5
anthony hitch <janthonyhitch@gmail.com>
2026-07-08 17:59:15 -0400

    c_quickstarts SIH update

--- c_quickstarts/SIH_QuickStart.h
+++ c_quickstarts/SIH_QuickStart.h
@@ -1,28 +1,29 @@
 // TO USE:
 // define anything like example.h before the define SIH_QUICKSTART
 //
-// #define SIH_QUICKSTART
+// #define SIH_QUICKSTART_IMPLEMENTATION
 // #include "SIH_QuickStart.h"
 //
 //
 //
 #ifndef SIH_QUICKSTART
 #define SIH_QUICKSTART
+//
+#ifndef CSTDIO_INCLUDE
+#define CSTDIO_INCLUDE
 #include <cstdio>
-//
-#ifndef EXAMPLE_IMPLEMENTATION
-#define EXAMPLE_IMPLEMENTATION
-#include "example.h"
-//
 #endif
+//
 typedef struct Foo {
 } Foo;
 void testFoo();
 
 // CODE
+// #ifndef to edit
 #ifdef SIH_QUICKSTART_IMPLEMENTATION
 void testFoo() { printf("foo"); }
 
 // IMPLEMENTATION
 #endif
+//
 #endif
